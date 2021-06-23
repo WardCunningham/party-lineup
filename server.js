a1361de05ffe0e79ead8c38b8aaaee433c962af5
Ward Cunningham <ward@c2.com>
2021-06-23 16:22:01 -0700

    json input and output

--- server.js
+++ server.js
@@ -1,41 +1,55 @@
 
 // https://deno.com/deploy/docs/hello-world
+// https://deno.com/deploy/docs/runtime-broadcast-channel
+
+const started = Date.now()
+const messages = []
+const channel = new BroadcastChannel("party-lineup")
+channel.onmessage = (event) => messages.push(event.data)
 
 addEventListener("fetch", (event) => event.respondWith(handle(event.request)))
-const started = Date.now()
 
-function handle(request) {
+
+async function handle(request) {
   let routes = {
+    "/send": send,
+    "/recall": recall,
     "/favicon.ico": flag,
-    "/new": random,
     "/": redirect
   }
   let client = request.headers.get("x-forwarded-for")
   let { pathname, search, origin } = new URL(request.url)
-  post([{eventType:'party-lineup', pathname, search, origin, client, started}])
+  post([{eventType:'PartyLineup', pathname, search, origin, client, started}])
   try {
-    return routes[pathname](search, origin)
+    return await routes[pathname]()
   } catch (err) {
     console.log(err)
     return new Response(`<pre>${err}</pre>`, {status:500})
   }
+
+  function redirect() {
+    return Response.redirect(`http://ward.asia.wiki.org/assets/pages/party-lineup/client.html`, 302)
+  }
+
+  async function send() {
+    let message = await request.json()
+    messages.push(message)
+    channel.postMessage(message)
+    let headers = {"access-control-allow-origin": "*"}
+    return new Response("ok", {headers})
+  }
+
+  function recall() {
+      let headers = {"content-type": "application/json", "access-control-allow-origin": "*"}
+      return new Response(JSON.stringify(messages,null,2), {headers})
+  }
+
+  function flag() {
+    let text = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">☎️</text></svg>`
+    return new Response(text, { headers: { "content-type": "image/svg+xml" } })
+  }
 }
 
-// https://css-tricks.com/emojis-as-favicons/
-
-function flag() {
-  let text = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🎯</text></svg>`
-  return new Response(text, { headers: { "content-type": "image/svg+xml" } })
-}
-
-function redirect(search, origin) {
-  return Response.redirect(`http://ward.asia.wiki.org/assets/pages/party-lineup/client.html`, 302)
-}
-
-function random(search, origin) {
-  let text = `Your number is ${Math.floor(Math.random()*10000)}. Write it down.`
-  return new Response(text, { headers: { "content-type": "text/plain" } })
-}
 
 function post(data) {
   fetch('https://insights-collector.newrelic.com/v1/accounts/3138524/events', {
